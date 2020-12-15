18f7f5025d3307cc990ca7a59dc1b01daa98a5e0
Eduardo P <zaexv@hotmail.com>
2020-12-15 17:48:51 +0100

    [ADD] Revision Model

--- marcianos/models.py
+++ marcianos/models.py
@@ -16,3 +16,10 @@ class Pasajero(models.Model):
     nombre = models.CharField(max_length=400)
     aeronave_id = models.ForeignKey(aeronave, on_delete=models.CASCADE,
         related_name = 'subido_en', null=True, blank=True)
+
+class Revision(models.Model):
+    nombre_revisor = models.CharField(max_length=400)
+    aeronave_id = models.ForeignKey(aeronave, on_delete=models.CASCADE
+        related_name = 'revisa_a', null=True, blank=True)
+    num_pasajeros = models.IntegerField()
+    fecha_revision = models.DateField()
